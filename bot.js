e99b4a8aa226342da81ac841e895a124b6d9ac4f
TheLar0 <thelar0@yahoo.com>
2014-08-20 20:52:20 -0500

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    name: 'Friendly Neighborhood Spiderman',
-    userid: function () {
-        return toId(this.name);
-    },
-    group: '&',
-    join: true,
-    rooms: ['lobby'],
